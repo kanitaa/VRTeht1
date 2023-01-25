b3512f076d43c4edc6285d7f60211690e4e45109
kanitaa <janita.kor@gmail.com>
2023-01-25 11:46:50 +0200

    left hand added

--- Assets/Scripts/Telekinesis.cs
+++ Assets/Scripts/Telekinesis.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Telekinesis : MonoBehaviour
 {
-    public InputActionReference telekinesisSelectReference = null;
+    public InputActionReference telekinesisRotationReference = null;
     bool overTelekinesisObject;
     bool grabObject;
+    [SerializeField]
+    InputActionReference selectAction;
+
+    [SerializeField]
+    float grabMaxRotation, grabMinRotation, releaseGrabRotation;
+
+    [SerializeField]
+    bool rightHand;
  
     public void OnHoverEntered(HoverEnterEventArgs args)
     {
         if (args.interactableObject.transform.gameObject.CompareTag("TelekinesisObject"))
         {
             overTelekinesisObject = true;
+            selectAction.action.Disable();
         }
       Debug.Log($"{args.interactorObject} hovered over {args.interactableObject}", this);
      
@@ -24,26 +35,48 @@ public class Telekinesis : MonoBehaviour
     public void OnHoverExited(HoverExitEventArgs args)
     {
         overTelekinesisObject = false;
+        selectAction.action.Enable();
         Debug.Log($"{args.interactorObject} stopped hovering over {args.interactableObject}", this);
    }
-
+  
     private void Update()
     {
-        //if rotation is more than min for grab
-        float value = telekinesisSelectReference.action.ReadValue<Quaternion>().eulerAngles.z;
-        if (value < 300 && value!=0 && value !>200 &&overTelekinesisObject && !grabObject)
+        if (rightHand)
         {
-            grabObject = true;
-            //grab object
-            GetComponent<XRRayInteractor>().hoverToSelect = true;
-            Debug.Log("GRAB");
-        }
-       if(value > 320 && overTelekinesisObject && grabObject)
+            //if rotation is good for grab
+            float value = telekinesisRotationReference.action.ReadValue<Quaternion>().eulerAngles.z;
+            if (value < grabMaxRotation && value != 0 && value! > grabMinRotation && overTelekinesisObject && !grabObject)
+            {
+                grabObject = true;
+                //grab object
+                GetComponent<XRRayInteractor>().hoverToSelect = true;
+                Debug.Log("GRAB");
+            }
+            if (value > releaseGrabRotation && overTelekinesisObject && grabObject)
+            {
+                grabObject = false;
+                GetComponent<XRRayInteractor>().hoverToSelect = false;
+                Debug.Log("RELEASE");
+            }
+
+        }else if (!rightHand)
         {
-            grabObject = false;
-            GetComponent<XRRayInteractor>().hoverToSelect = false;
-            Debug.Log("UNGRAB");
+            //if rotation is more than min for grab
+            float value = telekinesisRotationReference.action.ReadValue<Quaternion>().eulerAngles.z;
+            if (value > grabMinRotation && value <grabMaxRotation && overTelekinesisObject && !grabObject)
+            {
+                grabObject = true;
+                //grab object
+                GetComponent<XRRayInteractor>().hoverToSelect = true;
+                Debug.Log("GRAB");
+            }
+            if (value < releaseGrabRotation && overTelekinesisObject && grabObject)
+            {
+                grabObject = false;
+                GetComponent<XRRayInteractor>().hoverToSelect = false;
+                Debug.Log("RELEASE");
+            }
         }
-       
+
     }
 }
